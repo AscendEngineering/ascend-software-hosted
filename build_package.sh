80d78ea9cc0c48e1ddb446161d4b8fc0ba3f134e
Apoorv Thapliyal <apoorv1009@gmail.com>
2025-11-21 09:58:16 -0600

    add fixes

--- build_package.sh
+++ build_package.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
 set -e
 
-# Set email for GPG signing
+echo "Building package..."
+
 EMAIL="apoorv@ascendengineer.com"
-
-# Save the directory where this script was originally called
 ORIG_DIR="$(pwd)"
-
-# Directory where ascend-co lives
 ASCEND_CO_DIR="$HOME/ascend-co"
 
-# Run the build deb script
 cd "$ASCEND_CO_DIR"
 ./build_deb.sh
 
-# Find the built .deb file
-DEB_FILE=$(ls $HOME/ascend-co/*.deb | sort -r | head -n 1)
+DEB_FILE=$(ls "$ASCEND_CO_DIR"/*.deb | sort -r | head -n 1)
+echo "Found: $DEB_FILE"
 
-echo "Found package: $DEB_FILE"
-# Return to the original directory
 cd "$ORIG_DIR"
-
-# Copy the ascend-co_*.deb file into the current folder
 cp "$DEB_FILE" .
 
-# Generate Packages and Packages.gz
+echo "Updating package index..."
 dpkg-scanpackages --multiversion . > Packages
 gzip -k -f Packages
 
-# Generate Release, Release.gpg, and InRelease
+echo "Signing..."
 apt-ftparchive release . > Release
-gpg --yes --default-key "${EMAIL}" -abs -o Release.gpg Release
-gpg --yes --default-key "${EMAIL}" --clearsign -o InRelease Release
+gpg --yes --default-key "$EMAIL" -abs -o Release.gpg Release
+gpg --yes --default-key "$EMAIL" --clearsign -o InRelease Release
 
-# Git operations
+echo "Pushing..."
 git add .
 git commit -m "add fixes"
 git push -u origin main
+
+echo "Done."
