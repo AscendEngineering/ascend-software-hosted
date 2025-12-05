53a69b7357449bfa64082fd69824b9d40e73b274
Apoorv Thapliyal <apoorv1009@gmail.com>
2025-12-05 12:32:35 -0600

    add changes

--- build_package.sh
+++ build_package.sh
@@ -4,30 +4,33 @@ set -e
 echo "Building package..."
 
 EMAIL="apoorv@ascendengineer.com"
-ORIG_DIR="$(pwd)"
-ASCEND_CO_DIR="$HOME/ascend-co"
 
-cd "$ASCEND_CO_DIR"
-# ./build_deb.sh
+# Get the current directory name
+DIR="$(basename "$PWD")"
+echo "Current directory: $DIR"
 
-DEB_FILE=$(ls "$ASCEND_CO_DIR"/*.deb | sort -r | head -n 1)
-echo "Found: $DEB_FILE"
+# Set the Github username
+GITHUB_USERNAME="AscendEngineering"
 
-cd "$ORIG_DIR"
-cp "$DEB_FILE" .
-
+# Update APT package index files
 echo "Updating package index..."
 dpkg-scanpackages --multiversion . > Packages
 gzip -k -f Packages
 
+# Sign the release metadata and sign it
 echo "Signing..."
 apt-ftparchive release . > Release
 gpg --yes --default-key "$EMAIL" -abs -o Release.gpg Release
 gpg --yes --default-key "$EMAIL" --clearsign -o InRelease Release
 
-# echo "Pushing..."
+# Create the APT source list file
+echo "Updating source list..."
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/${DIR}.gpg] https://$GITHUB_USERNAME.github.io/$DIR ./" > ascend-software-hosted.list
+
+# Push changes to GitHub
+echo "Pushing to GitHub..."
 git add .
-git commit -m "add fixes"
+git commit -m "add changes"
 git push -u origin main
 
 echo "Done."
